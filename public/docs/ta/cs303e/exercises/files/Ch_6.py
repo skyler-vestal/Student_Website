f17fd269c272cbd83dcf0e4022a113fd2d96a197
skyler-vestal <dinoswarleaf@gmail.com>
2020-06-23 04:41:17 -0500

    Adds public deploy of update

---
+++ public/docs/ta/cs303e/exercises/files/Ch_6.py
@@ -0,0 +1,48 @@
+# Exercises for Chapter 6 of "Introduction to Programming using Python"
+# Solutions made by Skyler Vestal (Hook 'Em)
+
+# 6.1
+# CS 311 exercise =) - Prove that for any n the pentagonal number
+# is an integer
+def getPentagonalNumber(n):
+    return n * (3 * n - 1) // 2
+
+for i in range(10):
+    line_str = str(getPentagonalNumber(i * 10 + 1))
+    for j in range(2, 11):
+        line_str += ", " + str(getPentagonalNumber(i * 10 + j))
+    print(line_str)
+
+# 6.2
+def sumDigits(n):
+    sum = 0
+    while n > 0:
+        sum += n % 10
+        n //= 10
+    return sum
+
+sum = sumDigits(eval(input("Enter a number: ")))
+print("The sum of the digits is", sum)
+
+# 6.8
+def celsiusToFahrenheit(celsius):
+    return (9 / 5) * celsius + 32
+
+def fahrenheitToCelsius(fahrenheit):
+    return (5 / 9) * (fahrenheit - 32)
+
+c_temp = 40.0
+f_temp = 120.0
+print(format("Celsius", "<15"), format("Fahrenheit", "<10"),
+        "   |    ", format("Fahrenheit", "<15"), 
+        format("Celsius", "<10"), sep="")
+print()
+for i in range(10):
+    f_conv = celsiusToFahrenheit(c_temp)
+    c_conv = fahrenheitToCelsius(f_temp)
+    print(format(c_temp, "<15.1f"), 
+        format(f_conv, "<10.1f"), "   |   ", 
+        format(f_temp, "<15.1f"),
+        format(c_conv, "<10.1f"), sep = "")
+    c_temp -= 1
+    f_temp -= 10
